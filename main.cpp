1cff47d101016ddf640a80fe1f42fea5c3c5e003
Selsabeel A <90451756+SelsabeelA@users.noreply.github.com>
2022-10-22 17:56:27 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,6 +16,16 @@ int main(){
     BigDecimalInt num1("123456789012345678901234567890");
     BigDecimalInt num2("+113456789011345678901134567890");
     BigDecimalInt num3("-200000000000000000000000000000");
-    cout << "num1 = " << num1 << endl;
-    cout << "num2 = " << num2 << endl;
+    BigDecimalInt num4(21322323123);
+    BigDecimalInt num5(124343243279);
+    cout << "num1 = " << num1 << endl; // initialized from integer
+    cout << "num2 = " << num2 << endl; // positive sign
+    //cout << "num3 = " << num3 << endl; // negative
+    //cout << "num4 = " << num4 << endl; // initialized from integer
+    //cout << "num5 = " << num5 << endl;
+    cout << "num1 - num2 = " << (num1 - num2) << endl;
+    if(num1>num2){
+        cout << "num1 is larger";
+    }
+    else cout << "num2 is larger";
 }
